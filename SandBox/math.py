c9ebdbe8556678f51740b5ea488f1ff14b6e4de5
Never Mind <155938693+fcrownarisk@users.noreply.github.com>
2024-10-27 23:15:13 +0800

    Add files via upload

---
+++ SandBox/math.py
@@ -0,0 +1,64 @@
+import math
+def __init__(self):
+   pass
+def a(self):
+   a = 1
+   return a
+def b(self):
+   b = 2
+   return b
+def c(self):
+   c = 3
+   return c
+def d(self):    
+   d = 4
+   return d
+def e(self):
+   e = 5
+   return e
+def f(self):
+   f = 6
+   return f
+def g(self):
+   g = 7
+   return g
+def h(self):
+   h = 8
+   return h
+def i(self):
+   i = 9
+   return i
+def j(self):
+   j = 10
+   return j
+def k(self):
+   k = 11
+   return k
+def l(self):    
+   l = 12
+   return l
+def m(self):
+   m = 13
+   return m
+def n(self):
+   return 0
+result1 = math.sin(a)
+print(result1)
+result2 = math.cos(b)
+print(result2)
+result3 = math.asin(c)
+print(result3)
+result4 = math.acos(d)
+print(result4)
+result5 = math.asinh(e)
+print(result5)
+result6 = math.acosh(f)
+print(result6)
+result7 = math.sqrt(a)
+print(result7)
+result8 = math.pow(b)
+print(result8)
+result9 = math.exp(c)
+print(result9)
+result0 = math.log(d)
+print(result0)
