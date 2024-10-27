d5fa600697b5b0eec26cfd433f375c534c443e7f
Never Mind <155938693+fcrownarisk@users.noreply.github.com>
2024-10-27 23:37:19 +0800

    Add files via upload

---
+++ SandBox/interface.ts
@@ -0,0 +1,7 @@
+interface ICalculator{
+    constructor<aAbstractRange>(arg0: AbstractRange)
+}
+interface Calculator{
+    constructor(AILearner:ActiveXObject)
+          ActiveXObject: any
+}
---
+++ SandBox/namespace.ts
@@ -0,0 +1,15 @@
+const c = 299792458;
+namespace math{
+function sin(a: number){
+         Math.sin(a);
+    }
+    function cos(b: number) {
+        return Math.cos(b);
+    }
+    function asin(c: string){
+        return Math.asin;
+    }
+    function acos(d: string){
+        return Math.acos;
+    }
+}
